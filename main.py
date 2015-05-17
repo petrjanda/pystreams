75a4aee19db0f58d5056a63b41885388bc8a06aa
Petr Janda <petrjandacz@gmail.com>
2015-05-16 21:55:57 -0600

    find all words with length not being 3 and starting with B

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from flow import Flow
 from source import Source
 
 # Create source, flow and stream
-source = Source.from_list(["foo","bar","bazra", "aha", "fooba"])
+source = Source.from_list(["foo", "bar", "bazra", "aha", "fooba"])
 
-flow = Flow.filter(lambda i: len(i) != 3) \
-    .map(lambda i: "--> " + str(i))
+first = Flow.filter(lambda i: len(i) != 3) \
+    .map(lambda i: i.capitalize())
 
-stream = source.via(flow)
+second = Flow.filter(lambda i: i[0] == "B")
+
+stream = source \
+        .via(first) \
+        .via(second)
 
 # Benchmark
 start = time.time()
